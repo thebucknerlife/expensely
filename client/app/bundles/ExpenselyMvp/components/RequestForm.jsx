bb0d8f095d94a0fd33c66ea7bb814aa247f8638b
Greg Buckner <gbuckner23@gmail.com>
2017-01-11 23:38:28 -0800

    successfully converted requestform into stateless component :)

--- client/app/bundles/ExpenselyMvp/components/RequestForm.jsx
+++ client/app/bundles/ExpenselyMvp/components/RequestForm.jsx
@@ -1,24 +1,17 @@
 import React, { PropTypes } from 'react';
 
-export default class RequestForm extends React.Component {
-  constructor(props, _railsContext) {
-    super(props);
-    this.state = { items: this.props.items };
-  }
-
-  render() {
-    return (
-      <div>
-        <hr />
-        <form >
-          { this.props.items.length }
-        </form>
-      </div>
-    );
-  }
+const RequestForm = ({items}) => {
+  return (
+    <div>
+      <form >
+        { items.length }
+      </form>
+    </div>
+  );
 }
 
 Request.propTypes = {
   items: PropTypes.array,
 };
 
+export default RequestForm;
