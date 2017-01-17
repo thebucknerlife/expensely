eec11795ce1b3d8425d090d81694c034b30547f8
Greg Buckner <gbuckner23@gmail.com>
2017-01-17 15:10:07 -0800

    refactored bot with better organization and correct text

--- bots/expensely.rb
+++ bots/expensely.rb
@@ -1,8 +1,17 @@
 class Expensely < SlackRubyBot::Bot
-  CHARM = %w( Huzzah! Yas! Doit! )
+  CHARM = %w( Huzzah! Yas! Wootwoot! )
+
   command 'new' do |client, data, match|
     user = User.find_or_create_by(slack_id: data.user)
     request = Request.create(user: user)
-    client.say(channel: data.channel, text: "Let's get started! #{CHARM.sample} #{Rails.application.routes.url_helpers.edit_request_url(id: request.id, host: ENV['HOST'])}")
+    client.say(channel: data.channel, text: new_response(request))
+  end
+
+  def new_response(request)
+    "#{CHARM.sample} Receipts! My faaavorite. Let's get them uploaded here #{new_request_url(request.id)}"
+  end
+
+  def new_request_url(id)
+    Rails.application.routes.url_helpers.edit_request_url(id: request.id, host: ENV['HOST'])
   end
 end
