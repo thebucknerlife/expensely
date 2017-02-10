80556dd19207582afd8138de3f13168ff3060024
Greg Buckner <gbuckner23@gmail.com>
2017-02-10 13:49:04 -0800

    source_team?

--- bots/expensely/utils/user.rb
+++ bots/expensely/utils/user.rb
@@ -8,18 +8,22 @@ module Expensely
 
         user = ::User.find_or_initialize_by(slack_id: data.user)
         user.new = user.new_record?
-        set_attrs_from_slack(user, data.team) if user.new?
+        set_attrs_from_slack(user, data) if user.new?
 
         user
       end
 
-      def set_attrs_from_slack(user, team_id)
-        team = Team.find_by(team_id: team_id)
+      def set_attrs_from_slack(user, data)
+        team = Team.find_by(team_id: team_id(data))
         user.team = team
         response = web_client(team).users_info(user: user.slack_id)
         attrs = { 'slack_name' => response.user.name }
         attrs.merge!(response.user.profile.to_h.slice('email', 'first_name', 'last_name'))
         user.update(attrs)
+      end
+
+      def team_id(data)
+        data.team || data.source_team
       end
     end
   end
