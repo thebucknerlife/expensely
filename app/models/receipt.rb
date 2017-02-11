55328ebf730c3da5e70fbadb56027fa98bc81ecb
Greg Buckner <gbuckner23@gmail.com>
2017-02-10 21:39:26 -0800

    receipt spec

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,7 +2,30 @@ class Receipt < ApplicationRecord
   has_one :request_item
   #mount_uploader :image, ReceiptImageUploader
 
-  def url
+  def original_url
     cloudinary_json['url']
+  end
+
+  def url
+    if pdf?
+      pdf_to_image
+    else
+      cloudinary_json['url']
+    end
+  end
+
+  def pdf?
+    original_url.match(/\.pdf$/)
+  end
+
+  def pdf_to_image
+    original_url.gsub(/\.pdf$/,".jpg")
+  end
+
+  def thumbnail
+    @thumbnail ||= begin
+      insert_pos = url.index(/\/upload/)+7
+      url.insert(insert_pos, '/w_250,h_250,c_fit')
+    end
   end
 end
--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Receipt, type: :model do
+  let(:request) { Request.new }
+  let(:request_item) { RequestItem.new(request: request) }
+  let(:receipt) { Receipt.new(request_item: request_item, cloudinary_json: json) }
+  let(:json) { { url: 'http://res.cloudinary.com/dpquc2ssm/image/upload/v1486788807/x7w8cbcnj1eyz1nhnupf.jpeg' } }
+
+  it 'is a thing' do
+    expect(receipt).to be_valid?
+  end
+
+  it 'has a valid url' do
+    expect(receipt.url).to eq('')
+  end
+
+  it 'has a valid url' do
+    expect(receipt.thumbnail).to eq('')
+  end
+
+  context 'when a PDF' do
+    let(:json) { { url: 'http://res.cloudinary.com/dpquc2ssm/image/upload/v1486788807/x7w8cbcnj1eyz1nhnupf.pdf' } }
+    let(:receipt) { Receipt.new(request_item: request_item, cloudinary_json: json) }
+
+    it 'has a valid url' do
+      expect(receipt.url).to eq('')
+    end
+
+    it 'has a valid thumbnail' do
+      expect(receipt.thumbnail).to eq('')
+    end
+  end
 end
