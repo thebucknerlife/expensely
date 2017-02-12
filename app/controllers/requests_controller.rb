53db0b09550471e1e0ca68e2158ca1fc2184c46d
Greg Buckner <gbuckner23@gmail.com>
2017-02-11 18:10:48 -0800

    forgot to add thumbnail and original to request json

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -38,7 +38,7 @@ class RequestsController < ApplicationController
     {
       except: except, include: {
         request_items: { except: except, include: {
-          receipt: { only: [:id], methods: [:url] } }
+          receipt: { only: [:id], methods: [:url, :thumbnail, :original_url] } }
         }
       }
     }
