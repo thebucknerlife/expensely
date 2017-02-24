1def1e0a2d655945a2e88a8e57993e7a33d10df6
Greg Buckner <gbuckner23@gmail.com>
2017-02-23 23:42:57 -0800

    http auth

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,8 @@
 require("#{Rails.root}/bots/expensely/utils/bot_client")
 
 class RequestsController < ApplicationController
+  http_basic_authenticate_with name: "expensely", password: "nicoleisthebest525", only: [:index, :download]
+
   before_action :get_request, only: [:show, :update]
   before_action :verify_token, only: [:show, :update]
 
