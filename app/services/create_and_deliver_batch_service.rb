3631bf43bfd43f8c26075dd97af41d5041eea062
Greg Buckner <gbuckner23@gmail.com>
2017-06-21 16:50:46 -0700

    support for teams with no requests

--- app/services/create_and_deliver_batch_service.rb
+++ app/services/create_and_deliver_batch_service.rb
@@ -24,11 +24,15 @@ class CreateAndDeliverBatchService
     end
 
     public_ids = uploads.compact.map(&:public_id)
-    zip = zip_creator.create_and_save(public_ids: public_ids, team: team, batch: batch)
 
-    deliver_batch_email(zip.secure_url, batch)
+    if public_id.any?
+      zip = zip_creator.create_and_save(public_ids: public_ids, team: team, batch: batch)
+      deliver_batch_email(zip.secure_url, batch)
+      batch.requests.update_all(delivered_at: Time.now)
+    else
+      deliver_no_requests_email(team)
+    end
 
-    #batch.requests.update_all(delivered_at: Time.now)
     batch.save
   end
 
@@ -40,8 +44,11 @@ class CreateAndDeliverBatchService
     uploader.upload(rails_path: rails_path, user: user, team: user.team)
   end
 
-
   def deliver_batch_email(zip_url, batch)
     mailer.requests_batch_email(zip_url, batch).deliver_now
+  end
+
+  def deliver_no_requests_email(team)
+    mailer.no_requests_email(team).deliver_now
   end
 end
